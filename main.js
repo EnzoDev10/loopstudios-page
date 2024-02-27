57a50a754397c1ce560640ca6c4faeb608a14459
Enzo Cañete <enzodev4@gmail.com>
2024-02-26 22:20:56 -0300

    Small fix to mobile menu

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const body = document.querySelector("body");
 const menu = document.querySelector(".menu");
 const menuToggle = document.querySelector(".menu-btn");
 const menuIcon = menuToggle.querySelector("img");
@@ -7,9 +8,11 @@ const links = document.querySelectorAll(".nav-item-mob");
 
 menuToggle.addEventListener("click", () => {
     if (menuIcon.src.endsWith("hamburger.svg")) {
+        body.style.overflowY = "hidden";
         menu.classList.add("active");
         menuIcon.src = "images/icon-close.svg";
     } else {
+        body.style.overflowY = "visible";
         menu.classList.remove("active");
         menuIcon.src = "images/icon-hamburger.svg";
     }
